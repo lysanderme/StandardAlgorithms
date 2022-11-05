d02c9958e5ed915926fab3a1aebe6bd4a88002ac
Lysander Myrddin-Evans <myrddin-evans.l@etoncollege.org.uk>
2022-11-05 11:22:48 +0000

    added function to convert inches to centimetres

--- StandardAlgorithms/main.swift
+++ StandardAlgorithms/main.swift
@@ -7,5 +7,17 @@
 
 import Foundation
 
+func inchToCm(inch: Double) -> Double {
+    let cm: Double = inch * 2.54
+    return cm
+}
+
 print("Hello, Lysander!")
 
+let question = "How are you today"
+let message = "\(question)?"
+
+print(message)
+
+let centimetres = inchToCm(inch: 10)
+print(centimetres)
