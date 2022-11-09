cfaedbd790540743f462a16f35cb7f290529433b
Lysander Myrddin-Evans <myrddin-evans.l@etoncollege.org.uk>
2022-11-09 10:32:32 +0000

    made testing class and sorting class

---
+++ StandardAlgorithms/Sorting.swift
@@ -0,0 +1,15 @@
+//
+//  Sorting.swift
+//  StandardAlgorithms
+//
+//  Created by Myrddin-Evans, Lysander (JD) on 09/11/2022.
+//
+
+import Foundation
+
+class Sorting {
+    
+    func bubbleSort(data: [Int]) -> [Int] {
+        return data
+    }
+}
