17b98c83ab02001f4d82c9302fb68880327f6455
Lysander Myrddin-Evans <myrddin-evans/.l@etoncollege.org.uk>
2022-11-16 19:34:37 +0000

    Made merge sort and test for merge sort

--- StandardAlgorithms/Sorting.swift
+++ StandardAlgorithms/Sorting.swift
@@ -10,6 +10,58 @@ import Foundation
 class Sorting {
     
     func bubbleSort(data: [Int]) -> [Int] {
-        return data
+        var result = data
+        var swaps = 10
+        while swaps != 0 {
+            swaps = 0
+            for i in 0 ..< (result.count - 1) {
+                if result[i] > result[i+1] {
+                    let alt = result[i]
+                    result[i] = result[i+1]
+                    result[i+1] = alt
+                    swaps += 1
+                }
+            }
+        }
+        return result
+    }
+    
+    func mergeSort(data: [Int]) -> [Int] {
+        var result = data
+        if result.count > 1 {
+            let mid: Int = result.count / 2
+            var left = Array(result[0 ..< mid])
+            var right = Array(result[mid ..< result.count])
+            
+            left = mergeSort(left)
+            right = mergeSort(right)
+            
+            i = 0
+            j = 0
+            k = 0
+            
+            while i < left.count && j < right.count {
+                if left[i] <= right[i] {
+                    result[k] = left[i]
+                    i += 1
+                } else {
+                    result[k] = right[j]
+                    j += 1
+                }
+                k += 1
+            }
+            
+            while i < left.count {
+                result[k] = left[i]
+                i += 1
+                k += 1
+            }
+            
+            while j < right.count {
+                result[k] = right[j]
+                j += 1
+                k += 1
+        }
+            return result
     }
 }
--- StandardAlgoritmsTest/SortingTest.swift
+++ StandardAlgoritmsTest/SortingTest.swift
@@ -19,5 +19,16 @@ class SortingTest: XCTestCase {
         //assert - check the action behaved how we expected
         XCTAssertEqual(sortedArray, expected)
     }
+    
+    func testMergeSortWithUnsortedArrayOfIntegersReturnsSortedArray() {
+        //arrange - test setup
+        let unsortedArray = [3, 1, 10, 15, 4]
+        let expected = [1, 3, 4, 10, 15]
+        let sorting = Sorting()
+        //act - perform the action we want to test
+        let sortedArray = sorting.mergeSort(data: unsortedArray)
+        //assert - check the action behaved how we expected
+        XCTAssertEqual(sortedArray, expected)
+    }
 
 }
